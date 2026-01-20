2ff381138f5f2219704d933c1f331696bb4a058a
lstar888 <li7472001@gmail.com>
2026-01-20 12:38:49 +0800

    Update hysteria.sh

--- hysteria2-install-main/hy2/hysteria.sh
+++ hysteria2-install-main/hy2/hysteria.sh
@@ -222,7 +222,7 @@ insthysteria(){
     fi
     ${PACKAGE_INSTALL} curl wget sudo qrencode procps iptables-persistent netfilter-persistent
 
-
+    wget -N https://raw.githubusercontent.com/Misaka-blog/hysteria-install/main/hy2/install_server.sh
     bash install_server.sh
     rm -f install_server.sh
 
